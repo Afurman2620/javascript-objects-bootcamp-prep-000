da2326968ff30e460506c6f32fb2d0991856b63b
Artem  F <artemfm@gmail.com>
2021-02-03 01:24:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({Outkast: 'hey ya'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[Outkast] = songTitle
+  playlist[Outkast] = MsJackson
   return playlist
 }
